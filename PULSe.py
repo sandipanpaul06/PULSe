67b9325a8f9b6e7036a693523c8bfb90688bd850
sandipanpaul06 <109766583+sandipanpaul06@users.noreply.github.com>
2025-08-05 12:35:07 -0400

    Update PULSe.py

--- PULSe.py
+++ PULSe.py
@@ -34,22 +34,25 @@ def main():
     
     # Define all possible arguments for all modes
     parser.add_argument('-mode', required=True, help='Mode of operation')
-    parser.add_argument('-pref', type=str, help='File prefix for image_gen_ms and image_generation_vcf')
-    parser.add_argument('-out', type=str, help='Output file name for image_gen_ms')
-    parser.add_argument('-nHap', type=int, help='Number of haplotypes for image_gen_ms and image_generation_vcf')
+    parser.add_argument('-pref', type=str, help='File prefix for image_gen_ms and image_gen_vcf')
+    parser.add_argument('-out', type=str, help='Output file name')
+    parser.add_argument('-nHap', type=int, help='Number of haplotypes for image_gen_ms and image_gen_vcf')
     parser.add_argument('-subFolder', type=str, help='Subfolder name for image_gen_ms')
     parser.add_argument('-n', type=int, help='Number of files for image_gen_ms and image_generation_vcf')
     parser.add_argument('-start', type=int, help='Start number of files for image_gen_ms and image_generation_vcf')
-    parser.add_argument('-stop', type=int, help='Stop number of files for image_generation_vcf')
-    parser.add_argument('-imgDim', type=int, help='Image dimension for image_gen_ms and image_generation_vcf')
-    parser.add_argument('-fileName', type=str, help='File name for preprocess_vcf and prediction')
-    parser.add_argument('-outFolder', type=str, help='Output folder for preprocess_vcf')
-    parser.add_argument('-split', type=float, help='Train/test split for train mode')
-    parser.add_argument('-modelName', type=str, help='Name of model for train and prediction')
-    parser.add_argument('-Sw', type=str, help='Sweep file for train mode')
-    parser.add_argument('-Ne', type=str, help='Neutral file for train mode')
-    parser.add_argument('-subfolder', type=str, help='Subfolder for image_generation_vcf')
-    parser.add_argument('-outDat', type=str, help='Output dataset name for image_generation_vcf')
+    parser.add_argument('-stop', type=int, help='Stop number of files for image_gen_vcf')
+    parser.add_argument('-u', type=int, help='Number of samples on the unlabeled set')
+    parser.add_argument('-l', type=int, help='Number of samples on the labeled set')
+    parser.add_argument('-p', type=int, help='Percentage of positives in the unlabeled set')
+    parser.add_argument('-lp', type=str, help='Labeled positive filename prefix')
+    parser.add_argument('-pipeline', type=str, choices=['P1', 'P2'], help='P1 or P2')
+    parser.add_argument('-testcase', type=int, choices=[0, 1], help='0: simulated unlabeled set, 1: empirical unlabeled set')
+    parser.add_argument('-testname', type=str, help='Test name (train mode)')
+    parser.add_argument('--up', type=str, help='Unlabeled positive filename prefix (testcase 0 only, train mode)')
+    parser.add_argument('--un', type=str, help='Unlabeled negative filename prefix (testcase 0 only, train mode)')
+    parser.add_argument('--emp', type=str, help='Empirical filename (testcase 1 only, train mode)')
+    parser.add_argument('--C', type=str, help='C parameter (testcase 1 only, train mode)')
+    parser.add_argument('--L1', type=str, help='L1 parameter (testcase 1 only, train mode)')
 
     args = parser.parse_args()
     mode = args.mode
@@ -60,19 +63,35 @@ def main():
     # Validate arguments for each mode
     if mode == 'image_gen_ms':
         required_args = ['pref', 'out', 'nHap', 'subFolder', 'n', 'start']
-        invalid_args = ['fileName', 'outFolder', 'split', 'modelName', 'Sw', 'Ne', 'stop', 'outDat', 'subfolder', 'imgDim']
+        invalid_args = []
     elif mode == 'train':
-        required_args = ['Sw', 'Ne', 'split', 'modelName']
-        invalid_args = ['pref', 'out', 'nHap', 'subFolder', 'n', 'start', 'imgDim', 'stop', 'outDat', 'subfolder']
+        required_args = ['u', 'l', 'p', 'lp', 'pipeline', 'testcase', 'testname']
+        invalid_args = []
+
+        if args.testcase == 0:
+            if args.up is None or args.un is None:
+                print(f"Error: Missing required arguments for 'train' mode with testcase 0: --up and --un are required.")
+                sys.exit(1)
+            required_args.extend(['up', 'un'])
+            invalid_args.extend(['emp', 'C', 'L1'])
+        elif args.testcase == 1:
+            if args.emp is None or args.C is None or args.L1 is None:
+                print(f"Error: Missing required arguments for 'train' mode with testcase 1: --emp, --C, and --L1 are required.")
+                sys.exit(1)
+            required_args.extend(['emp', 'C', 'L1'])
+            invalid_args.extend(['up', 'un'])
+        else:
+            print(f"Error: Invalid value for -testcase. Must be 0 or 1.")
+            sys.exit(1)
     elif mode == 'preprocess_vcf':
         required_args = ['fileName', 'outFolder']
-        invalid_args = ['pref', 'out', 'nHap', 'subFolder', 'n', 'start', 'imgDim', 'split', 'modelName', 'Sw', 'Ne', 'stop', 'outDat', 'subfolder']
-    elif mode == 'image_generation_vcf':
+        invalid_args = []
+    elif mode == 'image_gen_vcf':
         required_args = ['subfolder', 'nHap', 'pref', 'start', 'stop', 'imgDim', 'outDat']
-        invalid_args = ['fileName', 'outFolder', 'split', 'modelName', 'Sw', 'Ne']
-    elif mode == 'prediction':
-        required_args = ['fileName', 'modelName']
-        invalid_args = ['pref', 'out', 'nHap', 'subFolder', 'n', 'start', 'imgDim', 'split', 'Sw', 'Ne', 'stop', 'outDat', 'subfolder']
+        invalid_args = []
+    elif mode == 'HOG':
+        required_args = ['fileName', 'pipeline']
+        invalid_args = []
     else:
         print(f"Error: Mode '{mode}' is invalid")
         sys.exit(1)
@@ -98,8 +117,8 @@ def main():
         'image_gen_ms': 'image_gen_ms.py',
         'train': 'train.py',
         'preprocess_vcf': 'preprocess_vcf.py',
-        'image_generation_vcf': 'image_generation_vcf.py',
-        'prediction': 'prediction.py'
+        'image_gen_vcf': 'image_gen_vcf.py',
+        'HOG': 'HOG.py'
     }
 
     if mode in script_mapping:
